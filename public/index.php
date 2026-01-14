884293201ad8fda709a6ea72e0121fa378122f66
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-01-14 22:51:05 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Core\Router;
+
+$router = new Router();
+require_once __DIR__ . '/../routes/web.php';
+
+$router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+?>
